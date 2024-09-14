497353070c957b32e9e82a1ddb954c9fe93dcc7d
Shunmugaraj Karuvanayagam <42828369+kshunmugaraj@users.noreply.github.com>
2024-09-14 12:10:11 +0530

    Longest sub-string without repeating characters

---
+++ longest-substring-without-repeating-characters.py
@@ -0,0 +1,14 @@
+def longest_sub_string(input_str:str):
+    l = 0
+    res = 0
+    charSet = set()
+    for r in range(len(input_str)):
+        while input_str[r] in charSet:
+            charSet.remove(input_str[l])
+            l += 1
+        charSet.add(input_str[r])
+        res = max(res, r-l+1)
+    return res
+
+if __name__ == "__main__":
+    print(longest_sub_string("abcdefababcdefghijka"))  
